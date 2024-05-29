04163a6838fd3f590047cfc1a286e1b787ea3923
TeufelJuri <jurgenmeti@gmail.com>
2024-05-29 22:18:47 +0200

    spring initialised

---
+++ src/main/java/com/example/TravellingAgency/demo/TravellingAgencyApplication.java
@@ -0,0 +1,13 @@
+package com.example.TravellingAgency.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravellingAgencyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravellingAgencyApplication.class, args);
+	}
+
+}
